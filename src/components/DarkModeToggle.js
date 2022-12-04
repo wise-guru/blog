56d35971e41c362f18c6c50eb750445b5503761a
wise-guru <wise.guru123@gmail.com>
2022-12-03 22:52:43 -0500

    Add darkmode

---
+++ src/components/DarkModeToggle.js
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+const DarkModeToggle = (props) => {
+  const { isDarkMode, setIsDarkMode } = props;
+
+  useEffect(() => {
+    setIsDarkMode(document.body.classList.contains("dark-mode"));
+  }, []);
+
+  const toggle = () => {
+    setIsDarkMode(!isDarkMode);
+    document.body.classList.toggle("dark-mode", !isDarkMode);
+    window.localStorage.setItem("prefersDarkMode", String(!isDarkMode));
+  };
+
+  return (
+    <div className="dark-mode-toggle">
+      <input
+        type="checkbox"
+        checked={isDarkMode}
+        onChange={toggle}
+        id="toggle"
+      />
+      <label htmlFor="toggle"></label>
+    </div>
+  );
+};
+
+export default DarkModeToggle;
